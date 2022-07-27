827c7286f901addde7fe6b88f79fbbcd4c8abba4
DESKTOP-JM65FNB\oscar <garnett284@gmail.com>
2022-07-28 06:53:44 +0800

    join room feature

--- index.js
+++ index.js
@@ -12,23 +12,37 @@ const io = new Server(server, {
   }
 });
 
+let users = []
+
 app.use("/", (req, res) => {
   res.send("Welcome!")
 })
 
+//run when a client connects
 io.on('connection', (socket) => {
-  console.log('a user connected');
   
-  //listen to if a client send a message to server
-  socket.on("chatting-message", (msg)=> {
-    console.log({msg})
-    console.log(socket.id)
-    io.emit("chatting-message", {user: msg.user, message: msg.message})
+  
+  socket.on('join', ({username, room}) => {
+    console.log(username, room)
+    const user = {
+      username,
+      id: socket.id,
+      room
+    }
+    users.push(user)
+    socket.join(room)
   })
 
+   //listen to if a client send a message to server
+  socket.on("chatting-message", (msg)=> {
+    const user = users.find(user => user.username === msg.username)
+    io.to(user.room).emit("chatting-message", msg)
+  })
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
+
+ 
 });
 
 server.listen(8080, () => {
